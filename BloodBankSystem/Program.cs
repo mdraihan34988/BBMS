3f39b44d7ee7803cdd80c844fb4e1ff51720ad57
mdraihan3434 <swammia908@gmail.com>
2021-05-03 18:12:25 +0600

    fix error

--- BloodBankSystem/Program.cs
+++ BloodBankSystem/Program.cs
@@ -18,7 +18,7 @@ namespace BloodBankSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Homepage());
+            Application.Run(new HomePage());
         }
     }
 }
